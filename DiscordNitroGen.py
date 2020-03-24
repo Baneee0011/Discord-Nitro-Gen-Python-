ad6536f3d1a84cb0a0648ab6ea5377910490d799
Baneee <62221872+Baneee0011@users.noreply.github.com>
2020-03-24 17:45:22 -0400

    Update DiscordNitroGen.py

--- DiscordNitroGen.py
+++ DiscordNitroGen.py
@@ -2,6 +2,9 @@ import random
 import os
 import string
 
+def random_char(y: int):
+    return ''.join(random.choice(string.ascii_letters) for x in range(y))
+
 random_string = random_char(16)
 link = "discord.gift/"
 print(link + random_string)

108a890c90813db8a11a6753539a73203d35ebeb
Baneee <62221872+Baneee0011@users.noreply.github.com>
2020-03-24 17:44:59 -0400

    Update DiscordNitroGen.py

--- DiscordNitroGen.py
+++ DiscordNitroGen.py
@@ -1,12 +1,8 @@
 import random
 import os
+import string
 
-def gen():
-    chars = ['a', 'b', 'c', 'd',  'e','f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-    '1','2','3','4','5','6','7','8','9','0'
-    ]
-    return "".join(random.choices(chars, k=16))
-
+random_string = random_char(16)
 link = "discord.gift/"
-print(link + gen())
+print(link + random_string)
 os.system('pause >NUL')
